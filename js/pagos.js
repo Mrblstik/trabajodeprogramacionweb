5f1046e9e51ad6c1ecbfd81d0806ce42c4af89b2
Mrblstik <simo.munoz@duocuc.cl>
2024-05-10 12:36:05 -0400

    arreglo de validacion de rut

--- js/pagos.js
+++ js/pagos.js
@@ -1,56 +1,52 @@
-
-
 $(document).ready(function() {
-    $('#payment-form').submit(function(event) {
-     
-      var formValid = true;
-      $(this).find('input, select').each(function() {
-        if (!$(this).val()) {
-          formValid = false;
-          return false; 
-        }
-      });
-      if (!formValid) {
-        alert('Por favor complete todos los campos.');
-        event.preventDefault(); 
-      } else {
-        
-        window.location.href = 'historialdecompras.html';
+  $('#payment-form').submit(function(event) {
+    var formValid = true;
+    $(this).find('input, select').each(function() {
+      if (!$(this).val()) {
+        formValid = false;
+        return false; 
       }
     });
-    
-    $('#numero-tarjeta').on('input', function() {
-      
-      var cardNumber = this.value.replace(/\D/g, '');
-      
-      cardNumber = cardNumber.replace(/(.{4})/g, '$1 ').trim();
-      
-      this.value = cardNumber;
-    });
-    
-    $('#fecha-vencimiento').on('input', function() {
-      this.value = this.value.replace(/\D/g, '');
-      if (this.value.length > 2) {
-        this.value = this.value.substring(0, 2) + '/' + this.value.substring(2);
-      }
-    });
-    
-    $('#cvv').on('input', function() {
-      
-      this.value = this.value.replace(/./g, '•');
-    });
-    
-    $('#codigo-postal').on('input', function() {
-      this.value = this.value.replace(/[^0-9]/g, '');
-    });
-    
-    $('#rut').on('input', function() {
-      this.value = this.value.replace(/[^\dKk]/g, '');
-      var rut = this.value.replace(/[.-]/g, '');
-      if (rut.length > 1) {
-        var formattedRut = rut.slice(0, -1).replace(/^0+/, '').replace(/(\d)(?=(\d{3})+(\.|$))/g, '$1.') + '-' + rut.slice(-1);
-        this.value = formattedRut;
-      }
-    });
+    // Verificar si el RUT tiene al menos 8 caracteres
+    if ($('#rut').val().replace(/[^\dKk]/g, '').length < 8) {
+      alert('El RUT debe tener al menos 8 caracteres.');
+      event.preventDefault();
+      return;
+    }
+    if (!formValid) {
+      alert('Por favor complete todos los campos.');
+      event.preventDefault(); 
+    } else {
+      window.location.href = 'historialdecompras.html';
+    }
   });
   
+  $('#numero-tarjeta').on('input', function() {
+    var cardNumber = this.value.replace(/\D/g, '');
+    cardNumber = cardNumber.replace(/(.{4})/g, '$1 ').trim();
+    this.value = cardNumber;
+  });
+  
+  $('#fecha-vencimiento').on('input', function() {
+    this.value = this.value.replace(/\D/g, '');
+    if (this.value.length > 2) {
+      this.value = this.value.substring(0, 2) + '/' + this.value.substring(2);
+    }
+  });
+  
+  $('#cvv').on('input', function() {
+    this.value = this.value.replace(/./g, '•');
+  });
+  
+  $('#codigo-postal').on('input', function() {
+    this.value = this.value.replace(/[^0-9]/g, '');
+  });
+  
+  $('#rut').on('input', function() {
+    var rut = this.value.replace(/[^\dKk]/g, '');
+    if (rut.length > 1) {
+      var formattedRut = rut.slice(0, -1).replace(/^0+/, '').replace(/(\d)(?=(\d{3})+(\.|$))/g, '$1.') + '-' + rut.slice(-1);
+      this.value = formattedRut;
+    }
+  });
+});
